7f987e5ecf6391f3e438482f692c70a6a792b65f
OlliJeCool <ollihomail@gmail.com>
2024-11-29 11:58:08 +0100

    updated day 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,39 @@
-'use client'
-
-import Heart from "@/components/day1/Heart";
+"use client";
 import Popup from "@/components/day1/Popup";
+import { motion } from "motion/react";
 import { useState } from "react";
 
 export default function Home() {
-  const [displayTimer, setDisplayTimer] = useState(false)
-  const [clickedHearts, setClickedHearts] = useState<boolean[]>(Array(10).fill(false))
-
-  const handleHeartClick = (hearIndex: number) => {
-   setClickedHearts((prevClicked) => prevClicked.map((flag, i) => (i === hearIndex ? true : flag))); 
-  };
+  const [displayTimer, setDisplayTimer] = useState(false);
 
   return (
-    <div className="w-[100vw] h-[100vh] bg-slate-200 flex justify-center items-center">
-      { 
-      !displayTimer ? <Popup setDisplayTimer={setDisplayTimer} /> : (
-      <div> 
-        <Heart isLiked={clickedHearts[1]} onLikeToggle={() => handleHeartClick(1)}/>
-      </div>
+    <div className="w-[100vw] h-[100vh] bg-gradient-to-tr from-sky-100 to-fuchsia-100 flex justify-center items-center">
+      {!displayTimer ? (
+        <Popup setDisplayTimer={setDisplayTimer} />
+      ) : (
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.3,
+            delay: 0.1,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+          className="w-1/2 h-1/3 flex flex-col items-center text-2xl"
+        >
+          <motion.p className="py-4">Dnešní písmeno je...</motion.p>
+          <motion.h2
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+            whileDrag={{ scale: 0.9, rotate: 10 }}
+            drag
+            dragSnapToOrigin
+            className="text-8xl font-extrabold bg-gradient-to-b from-violet-200 to-pink-200 bg-clip-text text-transparent py-2"
+          >
+            E
+          </motion.h2>
+          <p className="text-lg py-4">Snad ti bude chutnat pochoutka 😁</p>
+        </motion.div>
       )}
     </div>
   );
--- src/components/day1/Popup.tsx
+++ src/components/day1/Popup.tsx
@@ -1,21 +1,44 @@
-import React from 'react'
+import React from "react";
+import { motion } from "motion/react";
 
 interface popupPropData {
-    setDisplayTimer: (a: boolean) => void;
+  setDisplayTimer: (a: boolean) => void;
 }
 
-const Popup = ({setDisplayTimer} : popupPropData) => {
+const Popup = ({ setDisplayTimer }: popupPropData) => {
+  const handleClick = () => {
+    setTimeout(() => {
+      setDisplayTimer(true);
+    }, 100);
+  };
+
   return (
-    <div className="w-[20%] h-[20%] bg-slate-100 rounded-2xl flex items-center flex-col p-4 shadow-2xl shadow-rose-100">
-    <h1 className="text-2xl font-bold py-2">
-      {'Hola <3'}
-    </h1>
-    <p className="text-center py-4">
-      Tohle je trochu netradiční způsob TPZky, ale doufám, že se ti bude líbit.
-    </p>
-    <button onClick={() => setDisplayTimer(true)} className="bg-blue-300 py-2 w-1/3 rounded-lg hover:bg-blue-400 transition-all duration-1000 hover:scale-105">Klik :o</button>
-    </div>
-  )
-}
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
+      className="w-[30%] h-[30%] bg-white bg-opacity-25 border-blue-200 rounded-2xl flex items-center flex-col p-6"
+    >
+      <h1 className="text-2xl font-bold py-2">{"Hola <3"}</h1>
+      <p className="text-center py-4">
+        Tohle je trochu netradiční způsob TPZky, ale doufám, že se ti bude
+        líbit.
+      </p>
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 400, damping: 17 }}
+        onClick={() => handleClick()}
+        className="bg-blue-300 py-2 mt-4 w-1/3 rounded-lg hover:bg-blue-400"
+      >
+        Klik 🫣
+      </motion.button>
+    </motion.div>
+  );
+};
 
-export default Popup
+export default Popup;
