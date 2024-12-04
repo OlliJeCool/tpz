37fe69a858c820ed87985d880e81b57aa1404db6
OlliJeCool <ollihomail@gmail.com>
2024-12-04 23:46:28 +0100

    updatded

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["www.tpz-one.vercel.app"],
+  },
+};
 
 export default nextConfig;
