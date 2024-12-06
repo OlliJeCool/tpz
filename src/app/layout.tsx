84b70a9070dc31f924ad3bfd32258f996783d87f
OlliJeCool <ollihomail@gmail.com>
2024-12-06 23:19:42 +0100

    no menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Menu />
         {children}
       </body>
     </html>
